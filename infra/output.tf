98728c3509b738e4594a69cbba80e80fdc336390
Vagner <vagner.martins@tui.co.uk>
2021-08-20 14:04:30 -0300

    added output file

--- infra/output.tf
+++ infra/output.tf
@@ -1,11 +1,29 @@
-# output "tui_user_arn" {
-#   value = module.aws_resources.tui_user_arn
-# }
+output "key_name" {
+  value       = try(aws_key_pair.imported[0].key_name, aws_key_pair.generated[0].key_name, "")
+  description = "Name of SSH key"
+}
 
-# output "public_ssh_key" {
-#   value = module.aws_resources.public_ssh_key
-# }
+output "public_key" {
+  value       = try(aws_key_pair.imported[0].public_key, tls_private_key.default[0].public_key_openssh, "")
+  description = "Content of the generated public key"
+}
 
-# output "instance_ips" {
-#   value = aws_instance.publicInstanceTest.*.public_ip
-# }
+output "private_key" {
+  sensitive   = true
+  description = "Content of the generated private key"
+  value       = join("", tls_private_key.default.*.private_key_pem)
+}
+
+output "public_key_filename" {
+  description = "Public Key Filename"
+
+  # Prevent releasing filename to downstream consumers until file exists (aka not during plan):
+  value = length(join("", tls_private_key.default.*.public_key_openssh)) > 0 ? local.public_key_filename : local.public_key_filename
+}
+
+output "private_key_filename" {
+  description = "Private Key Filename"
+
+  # Prevent releasing filename to downstream consumers until file exists (aka not during plan):
+  value = length(join("", tls_private_key.default.*.public_key_openssh)) > 0 ? local.private_key_filename : local.private_key_filename
+}
