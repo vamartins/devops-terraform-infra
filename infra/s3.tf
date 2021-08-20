5338d4f7108397ecf336e849488ba5affe6cb7e2
Vagner <vagner.martins@tui.co.uk>
2021-08-20 10:10:26 -0300

    fixed gitlab-ci with new templates repo

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,6 +1,16 @@
-resource "aws_s3_bucket" "bucket" {
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.7.0"
+
   bucket = "vagner-samm"
   acl    = "private"
-
+  attach_deny_insecure_transport_policy = true
+  block_public_acls                     = true
+  block_public_policy                   = true
+  ignore_public_acls                    = true
+  restrict_public_buckets               = true
+  versioning = {
+    enabled = false
+  }
   tags = local.common_tags
 }
