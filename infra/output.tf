ddeb5e92b5900ca32105108578bb689c420b5706
Vagner <vagner.martins@tui.co.uk>
2021-09-03 08:32:17 -0300

    trigger43

--- infra/output.tf
+++ infra/output.tf
@@ -2,5 +2,5 @@ output "ec2_public_ip_awslab" {
   value = aws_instance.public_instance_awslab.*.public_ip
 }
 output "bucket_arn" {
-  value = local.awslab_s3_lambda_trigger.bucket_arn
+  value = module.awslab-s3-lambda-trigger.s3_bucket_arn
 }
